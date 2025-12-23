0771869d4255e15329846daf8d75cb058656ccd6
siduck <siduck@tutanota.com>
2025-12-23 16:00:22 +0530

    kekw

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import preset from './src/tailwind/preset'
+
+export default {
+  presets: [preset],
+  content: [
+    './index.html',
+    './App.vue',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './docs/**/*.{vue,js,ts,jsx,tsx}',
+    './docs/.vitepress/**/*.{vue,js,ts,jsx,tsx}',
+    // './docs/.vitepress/**/*.{vue,js,ts,jsx,tsx}',
+    './frappe/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
